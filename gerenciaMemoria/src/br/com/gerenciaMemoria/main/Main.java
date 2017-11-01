80ff32a9c18337a7251a2ce6bea51f3452a39e48
Pongelupe <pongelupe1998@gmail.com>
2017-10-31 23:33:20 -0200

    the output file was created

--- gerenciaMemoria/src/br/com/gerenciaMemoria/main/Main.java
+++ gerenciaMemoria/src/br/com/gerenciaMemoria/main/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import br.com.gerenciaMemoria.model.DadosEntradaAlgoritmo;
 import br.com.gerenciaMemoria.util.GerenciaEntradas;
+import br.com.gerenciaMemoria.util.GerenciaSaida;
 
 public class Main {
 
@@ -11,7 +12,8 @@ public class Main {
 		GerenciaEntradas gerenciaEntradas = new GerenciaEntradas("entrada1.txt");
 		try {
 			DadosEntradaAlgoritmo dadosEntrada = gerenciaEntradas.getDadosEntrada();
-			System.out.println(dadosEntrada);
+			System.out.println("Dados inseridos:\n\n" + dadosEntrada + "\n\nSaida\n\n\n");
+			new GerenciaSaida(4, 22122.321312311, 1, 3.58, 3.1444, 7, 9).exportarSaida();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
---
+++ gerenciaMemoria/src/br/com/gerenciaMemoria/util/GerenciaSaida.java
@@ -0,0 +1,56 @@
+package br.com.gerenciaMemoria.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
+
+public class GerenciaSaida {
+
+	private int requisicoes;
+	// Taxas de erro
+	private double fifo;
+	private double opt;
+	private double lru;
+	private double lfu;
+	private double mfu;
+	private double my;
+
+	private DecimalFormat formatter = new DecimalFormat("##.##");
+
+	public GerenciaSaida(int requisicoes, double fifo, double opt, double lru, double lfu, double mfu, double my) {
+		this.requisicoes = requisicoes;
+		this.fifo = fifo;
+		this.opt = opt;
+		this.lru = lru;
+		this.lfu = lfu;
+		this.mfu = mfu;
+		this.my = my;
+	}
+
+	public void exportarSaida() throws FileNotFoundException, UnsupportedEncodingException {
+
+		String saida = gerarSaida(new StringBuilder());
+		System.out.println(saida);
+
+		PrintWriter writer = new PrintWriter(new File("saida.txt"), "UTF-8");
+		writer.println(saida);
+		writer.close();
+
+	}
+
+	private String gerarSaida(StringBuilder sb) {
+		sb.append("Requisições=" + requisicoes);
+		sb.append("\nTaxasDeErros:");
+		sb.append("\nFIFO=" + formatter.format(fifo));
+		sb.append("\nOPT=" + formatter.format(opt));
+		sb.append("\nLRU=" + formatter.format(lru));
+		sb.append("\nLFU=" + formatter.format(lfu));
+		sb.append("\nMFU=" + formatter.format(mfu));
+		sb.append("\nMY=" + formatter.format(my));
+
+		return sb.toString().replaceAll("\\.", ",");
+	}
+
+}
