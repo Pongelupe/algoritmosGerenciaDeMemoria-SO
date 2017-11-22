f3556c3586a21ad7ce91ee91c4fd886a90c0dda2
Pongelupe <pongelupe1998@gmail.com>
2017-11-22 00:52:07 -0200

    Lfu with global allocation is done

--- gerenciaMemoria/src/br/com/gerenciaMemoria/algorithm/Lfu.java
+++ gerenciaMemoria/src/br/com/gerenciaMemoria/algorithm/Lfu.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import java.util.Queue;
 import java.util.TreeMap;
 
@@ -13,226 +16,119 @@ import br.com.gerenciaMemoria.model.NomeAlgoritmo;
 
 public class Lfu extends AlgoritmoDeGerencia {
 
+	private int totalErros = 0;
+	private int tamanhoQuadros = entrada.getTamanhoQuadros();
+	private int requisicoes = entrada.getSequencia().size();
+
 	public Lfu(DadosEntradaAlgoritmo entrada) {
 		super(entrada, NomeAlgoritmo.LFU);
 	}
 
-	
-	public boolean frequency(HashMap<String, TreeMap<Integer,Integer>> frequencia)
-	{
-		ArrayList<Integer> freq= new ArrayList<>();
-		TreeMap<Integer,Integer> aux= new TreeMap<>();
-		for(String process: frequencia.keySet())
-		{
-			aux=frequencia.get(process);
-			freq.addAll(aux.values());
-			
-		}
+	private double taxaErroGlobal() {
+		tamanhoQuadros = getTamMemoriaGlobal();
+		ArrayList<NoSequencia> memoria = new ArrayList<NoSequencia>(tamanhoQuadros);
+		HashMap<NoSequencia, Integer> mapFrequencia = new HashMap<>();
+		HashMap<NoSequencia, Integer> mapHit = new HashMap<>();
 
-		return Collections.max(freq).equals(Collections.min(freq));
-		
-	}
-	
+		for (int i = 0; i < requisicoes; i++) {
+			NoSequencia noAcessado = entrada.getSequencia().get(i);
 
-	public int retorne(HashMap<String, TreeMap<Integer,Integer>> frequencia, HashMap<String, TreeMap<Integer,Integer>> tempoEntrada )
-	{
-		TreeMap<Integer,Integer> t= new TreeMap<>();
-		ArrayList<Integer> a= new ArrayList<>();
-		ArrayList<Integer> freq= new ArrayList<>();
-		
-		for(String s:frequencia.keySet())
-		{
-			for(Integer i:frequencia.get(s).keySet())
-			{
-				freq.add(frequencia.get(s).get(i));
-				
-			}
-		}
-		
-		
-		for(String process: frequencia.keySet())
-		{
-		
-			for(Integer i:tempoEntrada.get(process).keySet())
-			{
-				
-				if(frequencia.get(process).get(i).equals(Collections.min(freq)))
-				{
-					
-					t.put(tempoEntrada.get(process).get(i), i);	
-					a.add(tempoEntrada.get(process).get(i));
+			if (memoria.size() == tamanhoQuadros) {
+				if (!isEmMemoria(memoria, noAcessado)) {
+					totalErros++;
+					NoSequencia key = getKeyRemovida(mapFrequencia, mapHit);
+
+					mapHit.remove(key);
+					mapHit.put(noAcessado, 0);
+
+					mapFrequencia.remove(key);
+					mapFrequencia.put(noAcessado, i);
+
+					memoria.remove(key);
+					memoria.add(noAcessado);
+				} else {
+					contabilizaHit(mapHit, noAcessado);
+				}
+
+			} else {
+				if (!isEmMemoria(memoria, noAcessado)) {
+					totalErros++;
+					memoria.add(noAcessado);
+					mapHit.put(noAcessado, 0);
+					mapFrequencia.put(noAcessado, i);
+				} else {
+					contabilizaHit(mapHit, noAcessado);
 				}
 			}
-		
-		}
-		
-		return t.get(Collections.min(a));
-	}
-	
-	public double taxaErroGlobal() {
-		int totalErros = 0;
-		HashMap<String, Queue<Integer>> dadosFila= new HashMap<>();
-		ArrayList<Integer> filaPaginas=new ArrayList<>();
-		ArrayList<String> filaProcessos= new ArrayList<>();
-		HashMap<String, TreeMap<Integer,Integer>> frequencia= new HashMap<>();
-		HashMap<String, TreeMap<Integer,Integer>> tempoEntrada= new HashMap<>();
-		int totalQuadros=0;
-	
-		
-		if (entrada.getAlocacao().equals("Igual")) 
-		{
-			totalQuadros=entrada.getTamanhoQuadros()/entrada.getProcessos().size();
-			totalQuadros=totalQuadros*entrada.getProcessos().size();
-		} 
-		
-		
-		else if(entrada.getAlocacao().equals("Proporcional"))
-		{
-			
-			int totalPaginas=0;
-			for(int i=0;i<entrada.getProcessos().size();i++)
-			{
-				totalPaginas+=entrada.getProcessos().get(i).getNumPaginas();
-			}
-		
-			int soma=0;
-			for(int i=0;i<entrada.getProcessos().size();i++)
-			{
-				int totalPaginasPr=entrada.getProcessos().get(i).getNumPaginas();
-				totalQuadros=(int) ((double)totalPaginasPr/totalPaginas*totalQuadros);
-				System.out.println(totalQuadros);
-				soma+=totalQuadros;
-			}
-			
-			totalQuadros=soma;
-		}
-			
-		
-			
-		for(int i=0;i<entrada.getProcessos().size();i++)
-		{
-			frequencia.put(entrada.getProcessos().get(i).getNome(), new TreeMap<>());
-			tempoEntrada.put(entrada.getProcessos().get(i).getNome(), new TreeMap<>());
-			dadosFila.put(entrada.getProcessos().get(i).getNome(), new LinkedList<>());
-		}
-		
-		for (int i = 0; i < entrada.getSequencia().size(); i++) 
-		{
-			String nomeProcesso = entrada.getSequencia().get(i).getProcesso();
-			int pagAcessada = entrada.getSequencia().get(i).getPaginaAcessada();
-		
-			if(dadosFila.get(nomeProcesso).contains(pagAcessada)==false)
-			{
-				totalErros++;
-				
-				if(filaPaginas.size()<totalQuadros)
-				{
-					
-					filaPaginas.add(pagAcessada);
-					filaProcessos.add(nomeProcesso);		
-					tempoEntrada.get(nomeProcesso).put(pagAcessada,i);
-					frequencia.get(nomeProcesso).put(pagAcessada,-1);
-					dadosFila.get(nomeProcesso).add(pagAcessada);
-				}
-				
-				else {
-					
-					if(frequency(frequencia)==true)
-					{
-						int pagRemovida=filaPaginas.remove(0);
-						String processoRemovido=filaProcessos.remove(0);
-						frequencia.get(processoRemovido).remove(pagRemovida);
-						tempoEntrada.get(processoRemovido).remove(pagRemovida);
-						dadosFila.get(processoRemovido).remove(pagRemovida);
-	
-						frequencia.get(nomeProcesso).put(pagAcessada,-1);
-						filaPaginas.add(pagAcessada);
-						tempoEntrada.get(nomeProcesso).put(pagAcessada,i);
-						filaProcessos.add(nomeProcesso);
-						dadosFila.get(nomeProcesso).add(pagAcessada);
-					} 
-					else
-					{
-		
-						int pagRemovida=retorne(frequencia,tempoEntrada);
-						String processoRemovido=filaProcessos.remove(filaPaginas.indexOf(pagRemovida));
-						filaPaginas.remove(filaPaginas.indexOf(pagRemovida));
-						frequencia.get(processoRemovido).remove(pagRemovida);
-						tempoEntrada.get(processoRemovido).remove(pagRemovida);
-						dadosFila.get(processoRemovido).remove(pagRemovida);
-					
-						frequencia.get(nomeProcesso).put(pagAcessada,-1);
-						filaPaginas.add(pagAcessada);
-						tempoEntrada.get(nomeProcesso).put(pagAcessada,i);
-						filaProcessos.add(nomeProcesso);
-						dadosFila.get(nomeProcesso).add(pagAcessada);
-					}
-					
-					
-				}
-				
-				
-			}
-			
-			else 
-			{
-				int freq=frequencia.get(nomeProcesso).get(pagAcessada);
-				freq++;
-				frequencia.get(nomeProcesso).put(pagAcessada, freq);
-			}
-			
 
 		}
-			System.out.println(totalErros+" "+entrada.getSequencia().size());
-			return (double) totalErros / entrada.getNumeroRequisicoes();
 
+		return (double) totalErros / requisicoes;
 	}
-		
+
+	private NoSequencia getKeyRemovida(HashMap<NoSequencia, Integer> mapFrequencia,
+			HashMap<NoSequencia, Integer> mapHit) {
+
+		NoSequencia key = null;
+
+		Entry<NoSequencia, Integer> entryMenorValor = mapHit.entrySet().stream()
+				.max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get();
+
+		List<Entry<NoSequencia, Integer>> entriesCandidatos = mapHit.entrySet().stream()
+				.filter(entry -> entry.getValue().equals(entryMenorValor.getValue())).collect(Collectors.toList());
+
+		boolean precisaDesempate = entriesCandidatos.size() > 1;
+
+		if (precisaDesempate) {
+
+			ArrayList<NoSequencia> nosCandidatos = new ArrayList<NoSequencia>();
+			entriesCandidatos.stream().forEach(no -> nosCandidatos.add(no.getKey()));
+			key = mapFrequencia.entrySet().stream().filter(entry -> nosCandidatos.contains(entry.getKey()))
+					.min((entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue())).get().getKey();
+		} else
+			key = entryMenorValor.getKey();
+
+		return key;
+	}
+
+	// Alocacao igual substitui��o local
+
 	public double taxaErroIgual() {
 		int totalErros = 0;
 		int totalQuadros = 0;
 		HashMap<String, Queue<Integer>> filaPaginas = new HashMap<>();
 		HashMap<String, HashMap<Integer, Integer>> frequencia = new HashMap<>();
-		HashMap<String, HashMap<Integer,Integer>> tempoEntrada = new HashMap<>(); 
-		HashMap<String,Integer> tamanho= new HashMap<>();
-		
-	
-		if (entrada.getAlocacao().equals("Igual")) 
-		{
-			totalQuadros=entrada.getTamanhoQuadros()/entrada.getProcessos().size();
-			
-			
-			for(int i=0;i<entrada.getProcessos().size();i++)
-			{
+		HashMap<String, HashMap<Integer, Integer>> tempoEntrada = new HashMap<>();
+		HashMap<String, Integer> tamanho = new HashMap<>();
+
+		if (entrada.getAlocacao().equals("Igual")) {
+			totalQuadros = entrada.getTamanhoQuadros() / entrada.getProcessos().size();
+
+			for (int i = 0; i < entrada.getProcessos().size(); i++) {
 				tamanho.put(entrada.getProcessos().get(i).getNome(), totalQuadros);
-				
+
 			}
-				
-		} 
-		else if(entrada.getAlocacao().equals("Proporcional")){
-			
-			int totalPaginas=0;
-			for(int i=0;i<entrada.getProcessos().size();i++)
-			{
-				totalPaginas+=entrada.getProcessos().get(i).getNumPaginas();
-				
+
+		} else if (entrada.getAlocacao().equals("Proporcional")) {
+
+			int totalPaginas = 0;
+			for (int i = 0; i < entrada.getProcessos().size(); i++) {
+				totalPaginas += entrada.getProcessos().get(i).getNumPaginas();
+
 			}
-			
-			for(int i=0;i<entrada.getProcessos().size();i++)
-			{
-				
-				int totalPaginasPr=entrada.getProcessos().get(i).getNumPaginas();
-				totalQuadros=entrada.getTamanhoQuadros();
-				totalQuadros=(int) ((double)totalPaginasPr/totalPaginas*totalQuadros);
+
+			for (int i = 0; i < entrada.getProcessos().size(); i++) {
+
+				int totalPaginasPr = entrada.getProcessos().get(i).getNumPaginas();
+				totalQuadros = entrada.getTamanhoQuadros();
+				totalQuadros = (int) ((double) totalPaginasPr / totalPaginas * totalQuadros);
 				tamanho.put(entrada.getProcessos().get(i).getNome(), totalQuadros);
-				
+
 			}
-			
+
 		}
 
-
-		for (int i = 0; i < entrada.getProcessos().size(); i++) 
-		{
+		for (int i = 0; i < entrada.getProcessos().size(); i++) {
 			filaPaginas.put(entrada.getProcessos().get(i).getNome(), new LinkedList<>());
 			frequencia.put(entrada.getProcessos().get(i).getNome(), new HashMap<>());
 			tempoEntrada.put(entrada.getProcessos().get(i).getNome(), new HashMap<>());
@@ -242,46 +138,38 @@ public class Lfu extends AlgoritmoDeGerencia {
 			String nomeProcesso = entrada.getSequencia().get(i).getProcesso();
 			int pagAcessada = entrada.getSequencia().get(i).getPaginaAcessada();
 
-			if (filaPaginas.get(nomeProcesso).contains(pagAcessada) == false) 
-			{
+			if (filaPaginas.get(nomeProcesso).contains(pagAcessada) == false) {
 				totalErros++;
 
-				if (filaPaginas.get(nomeProcesso).size()<tamanho.get(nomeProcesso)) 
-				{
+				if (filaPaginas.get(nomeProcesso).size() < tamanho.get(nomeProcesso)) {
 
 					filaPaginas.get(nomeProcesso).add(pagAcessada);
 					frequencia.get(nomeProcesso).put(pagAcessada, 0);
 					tempoEntrada.get(nomeProcesso).put(pagAcessada, i);
-						
-				} else 
-				{
-					if (frequency(frequencia, nomeProcesso)==false) 
-					{
+
+				} else {
+					if (frequency(frequencia, nomeProcesso) == false) {
 
 						int pagRemovida = filaPaginas.get(nomeProcesso).remove();
 						frequencia.get(nomeProcesso).remove(pagRemovida);
 						tempoEntrada.get(nomeProcesso).remove(pagRemovida);
 						filaPaginas.get(nomeProcesso).add(pagAcessada);
-						frequencia.get(nomeProcesso).put(pagAcessada,0);
-						tempoEntrada.get(nomeProcesso).put(pagAcessada,i);
-						
-					
-					} else 
-					{
+						frequencia.get(nomeProcesso).put(pagAcessada, 0);
+						tempoEntrada.get(nomeProcesso).put(pagAcessada, i);
 
-						int pagRemovida=retorne(frequencia,tempoEntrada,nomeProcesso);						
+					} else {
+
+						int pagRemovida = retorne(frequencia, tempoEntrada, nomeProcesso);
 						filaPaginas.get(nomeProcesso).remove(pagRemovida);
 						frequencia.get(nomeProcesso).remove(pagRemovida);
 						tempoEntrada.get(nomeProcesso).remove(pagRemovida);
 						filaPaginas.get(nomeProcesso).add(pagAcessada);
-						frequencia.get(nomeProcesso).put(pagAcessada,0);
-						tempoEntrada.get(nomeProcesso).put(pagAcessada,i);
-						
-						
+						frequencia.get(nomeProcesso).put(pagAcessada, 0);
+						tempoEntrada.get(nomeProcesso).put(pagAcessada, i);
+
 					}
 				}
-			} else 
-			{
+			} else {
 
 				int freq = frequencia.get(nomeProcesso).get(pagAcessada);
 				freq++;
@@ -299,11 +187,10 @@ public class Lfu extends AlgoritmoDeGerencia {
 		HashMap<Integer, Integer> freq = map.get(process);
 		ArrayList<Integer> a = new ArrayList<>();
 
-		
 		for (Integer i : freq.keySet()) {
 			a.add(freq.get(i));
 		}
-			
+
 		int primeiro = a.get(0);
 		for (Integer i : a) {
 			if (i != primeiro) {
@@ -311,22 +198,23 @@ public class Lfu extends AlgoritmoDeGerencia {
 			}
 
 		}
-		
+
 		return chave;
 	}
 
-	private int retorne(HashMap<String, HashMap<Integer, Integer>> map,HashMap<String, HashMap<Integer, Integer>> time ,String process) {
+	private int retorne(HashMap<String, HashMap<Integer, Integer>> map, HashMap<String, HashMap<Integer, Integer>> time,
+			String process) {
 
 		HashMap<Integer, Integer> freq = map.get(process);
 		ArrayList<Integer> a = new ArrayList<>();
-		HashMap<Integer, Integer> timeEntry=time.get(process);
-		
+		HashMap<Integer, Integer> timeEntry = time.get(process);
+
 		for (Integer i : freq.keySet()) {
 			a.add(freq.get(i));
 		}
 
-		int menorFrequencia=Collections.min(a);
-		TreeMap<Integer,Integer> t= new TreeMap<>();
+		int menorFrequencia = Collections.min(a);
+		TreeMap<Integer, Integer> t = new TreeMap<>();
 		for (Integer i : freq.keySet()) {
 			if (freq.get(i) == menorFrequencia) {
 				t.put(timeEntry.get(i), i);
@@ -335,6 +223,10 @@ public class Lfu extends AlgoritmoDeGerencia {
 		}
 
 		return t.get(t.lastKey());
+	}
+
+	private void contabilizaHit(HashMap<NoSequencia, Integer> map, NoSequencia noAcessado) {
+		map.put(noAcessado, map.get(noAcessado) - 1);
 	}
 
 	@Override
